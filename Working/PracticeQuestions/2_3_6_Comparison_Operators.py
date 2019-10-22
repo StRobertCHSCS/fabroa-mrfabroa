8ae9cabaa43fe8836bc1adb363e9265b4ea79cad
mrfabroa <eric.fabroa@ycdsbk12.ca>
2019-10-22 09:30:05 -0400

    comparison example

---
+++ Working/PracticeQuestions/2_3_6_Comparison_Operators.py
@@ -0,0 +1,8 @@
+reservation_name = "Shonda"
+name = input("Enter a name: ")
+
+if reservation_name == name:
+    print("Right this way!")
+else:
+    print("Sorry, we don't have a reservation under that name.")
+
