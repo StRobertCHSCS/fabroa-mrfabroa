23d7eeb0496c5c3b1faae1949ebeae08de2f95d0
mrfabroa <eric.fabroa@ycdsbk12.ca>
2019-11-27 09:28:15 -0500

    2_8_7 solution

---
+++ Masters_DO_NOT_EDIT/2_ControlStructures/2_8_For_loops/Solutions/2_8_7_for_loops.py
@@ -0,0 +1,17 @@
+"""Write a cash register program that allows the user to enter the price AND quantity for 3 products.  
+Output the total cost with 13% tax applied rounded to 2 decimals.  
+(use `round(num,2)` to round `num` to 2 decimal places )"""
+
+# initialize the total
+total = 0
+
+# get the products and quantity and add them to the total
+for i in range (1, 4):
+    price = float(input("Enter the price of product "+str(i)+": "))
+    qty = int(input("Enter the quantity of product "+str(i)+": "))
+
+    total = total + (price * qty)
+
+print(round(total,2))
+
+
